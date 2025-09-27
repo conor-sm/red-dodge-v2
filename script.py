b9de8af8dee6a91a0a554fa111a4984b74c01af5
Conor <220956160+conor-sm@users.noreply.github.com>
2025-09-27 20:58:19 +0100

    Final push, WARNING - Crashes computer and locks GUI

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ pygame.init()
 clock = pygame.time.Clock()
 S_WIDTH, S_HEIGHT = 400, 900
 screen = pygame.display.set_mode((S_WIDTH, S_HEIGHT))
+score = 0
 
 running = True
 menu_active = True
@@ -13,9 +14,9 @@ class Player():
     def __init__(self, x):
         self.player_obj = pygame.Rect(0, 800, 100, 100)
         self.x = x
-        self.player_speed = 5
+        self.player_speed = 6
 
-    def update(self, x):
+    def update(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -31,17 +32,75 @@ class Player():
         pygame.draw.rect(screen, (0, 255, 0), self.player_obj)
 
 player = Player(0)
+
+class Enemy():
+    def __init__(self):
+        self.Y = 0
+        self.enemy_speed = 7
+        self.gravity = 0.5
+        self.velocity = 0
+        self.enemy_object = pygame.Rect(random.randint(0, S_WIDTH - 50), 0, 50, 50)
+    
+    def produce_enemy(self):
+        self.enemy_object = pygame.Rect(random.randint(0, S_WIDTH - 50), 0, 50, 50)
+        self.Y = 0
+        self.velocity = 0
+
+    def status_check(self):
+        global score, running, menu_active, game_active
+        if self.enemy_object.colliderect(player.player_obj):
+            score = 0
+            game_active = False
+            menu_active = True
+        
+        if self.enemy_object.y > S_HEIGHT:
+            score += 1
+            self.produce_enemy()
+
+    def update(self):
+        self.velocity += self.gravity
+
+        if self.velocity > 12:
+            self.velocity = 12
+
+        self.Y += self.velocity
+        self.enemy_object.y = int(self.Y)
+
+    def draw(self):
+        pygame.draw.rect(screen, (255, 0, 0), self.enemy_object)
+
+enemy = Enemy()
+
+def game():
+    screen.fill((0, 0, 0))
+    player.draw()
+    enemy.draw()
+    enemy.update()
+    enemy.status_check()
+    player.update()
+    pygame.display.update()
+    clock.tick(60)
+    pygame.display.set_caption(f"Score: {score}")
+
+def menu():
+    screen.fill((0, 100, 0))
+    pygame.display.set_caption("ENTER to begin")
+        
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
             running = False
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN and menu_active:
+                menu_active = False
+                game_active = True
 
-    screen.fill((0, 0, 0))
-    player.draw()
-    player.update(player.x)
-    pygame.display.update()
-    clock.tick(60)
-    pygame.display.set_caption(f"FPS: {int(clock.get_fps())}")
+    if game_active:
+        game()
+
+    elif menu_active:
+        menu()
+
 
 pygame.quit()
